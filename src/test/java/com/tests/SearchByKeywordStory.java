889ab304e1c822d4fcc6ebf71fdf5573ddcf7370
ramonaZmole <ramona.zmole@evozon.com>
2015-06-09 12:11:45 +0300

    Updated test runner and added gitignore

--- src/test/java/com/tests/SearchByKeywordStory.java
+++ src/test/java/com/tests/SearchByKeywordStory.java
@@ -1,13 +1,12 @@
 package com.tests;
 
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Issue;
+import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.ManagedPages;
 import net.thucydides.core.annotations.Pending;
 import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.annotations.Story;
 import net.thucydides.core.pages.Pages;
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.junit.runners.ThucydidesRunner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +14,7 @@ import org.openqa.selenium.WebDriver;
 
 import com.steps.EndUserSteps;
 
-@RunWith(ThucydidesRunner.class)
+@RunWith(SerenityRunner.class)
 public class SearchByKeywordStory {
 
     @Managed(uniqueSession = true)
